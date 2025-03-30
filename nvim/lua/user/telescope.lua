59da8b758852a8a96876b458a1d57a839e729d98
anlai007 <anlai007@umn.edu>
2025-03-30 15:40:32 -0500

    Add hotkey for telescope to search every file from my home dir.

--- nvim/lua/user/telescope.lua
+++ nvim/lua/user/telescope.lua
@@ -1,7 +1,7 @@
 local builtin_status, builtin = pcall(require, "telescope.builtin")
 
 if not builtin_status then
-	return
+  return
 end
 
 vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
@@ -9,9 +9,17 @@ vim.keymap.set("n", "<C-p>", builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>ht', builtin.help_tags, {})
-vim.keymap.set('n', '<leader>bf', builtin.current_buffer_fuzzy_find	, {})
-
-
+vim.keymap.set('n', '<leader>bf', builtin.current_buffer_fuzzy_find, {})
 
 require('telescope').load_extension('fzf')
 
+
+--[[Search for a file starting at my home directory.  ]]
+vim.keymap.set('n', '<leader>F', function()
+  require('telescope.builtin').find_files({
+    prompt_title = "Global File Search",
+    cwd = "~",     -- root of the filesystem
+    hidden = true, -- include hidden files
+    follow = true, -- follow symlinks
+  })
+end, { desc = "Find files (global)" })
