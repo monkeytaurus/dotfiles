f104311f38e800dcf69ef9d05deb691fd817288f
anlai007 <anlai007@umn.edu>
2025-04-05 15:04:39 -0500

    Fix another error. Change tsserver to ts_ls

--- nvim/lua/user/lsp.lua
+++ nvim/lua/user/lsp.lua
@@ -19,7 +19,7 @@ local on_attach = function(client, bufnr)
 
 
 
-  lspconfig.tsserver.setup({
+  lspconfig.ts_ls.setup({
     on_attach = on_attach,
     capabilities = capabilities,
     root_dir = lspconfig.util.root_pattern("package.json", "tsconfig.json", ".git"),
