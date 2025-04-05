cb2aa10bd2f1caff307113e47bdd4620a03f04ec
anlai007 <anlai007@umn.edu>
2025-04-05 12:01:22 -0500

    Add keybinds for harpoon

--- nvim/lua/user/harpoon.lua
+++ nvim/lua/user/harpoon.lua
@@ -19,18 +19,32 @@ local function toggle_telescope(harpoon_files)
     }):find()
 end
 
-vim.keymap.set("n", "<C-e>", function() toggle_telescope(harpoon:list()) end,
+-- Allows you to see buffer files from Harpoon
+vim.keymap.set("n", "<leader>E", function() toggle_telescope(harpoon:list()) end,
     { desc = "Open harpoon window" })
+-- Note: To write a change to the harpoon menu, use :w
 
 
 vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
 vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+-- Note: To write a change to the harpoon menu, use :w
 
-vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
-vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
-vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
-vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
+vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end)
+
+
+-- vim.keymap.set("n", "<C-i>", function() harpoon:list():select(1) end)
+-- vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
+-- vim.keymap.set("n", "<C-l>", function() harpoon:list():select(3) end)
+-- vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
+
 
 -- Toggle previous & next buffers stored within Harpoon list
-vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
-vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
+-- vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
+-- vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
+
+vim.keymap.set("n", "<leader>P", function() harpoon:list():prev() end)
+vim.keymap.set("n", "<leader>N", function() harpoon:list():next() end)
+
